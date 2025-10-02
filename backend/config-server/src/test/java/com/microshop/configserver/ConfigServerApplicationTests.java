55ba45becdceb00038cb95fce9fd277098aea8a7
TienDattttt <nguyentiendat201204@gmail.com>
2025-10-02 21:10:09 +0700

    setup backend

---
+++ backend/config-server/src/test/java/com/microshop/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microshop.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
